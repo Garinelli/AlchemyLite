8c749006391c58a32fb09aa6ff365515c7cb14bd
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-11-01 14:41:16 +0700

    Add base config for configs.

--- easyalchemy/config.py
+++ easyalchemy/config.py
@@ -1,8 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from abc import ABC, abstractmethod
 
-class SyncConfig:
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import sessionmaker
+
+
+class BaseConfig(ABC):
     def __init__(self, db_host: str, db_port: str, db_user: str, db_pass: str,
                  db_name: str) -> None:
         self.db_host = db_host
@@ -11,6 +14,17 @@ class SyncConfig:
         self.db_pass = db_pass
         self.db_name = db_name
 
+    @property
+    @abstractmethod
+    def DATABASE_URL(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_session(self):
+        pass
+
+
+class SyncConfig(BaseConfig):
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+psycopg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
@@ -24,14 +38,7 @@ class SyncConfig:
         return session
 
 
-class AsyncConfig:
-    def __init__(self, db_host: str, db_port: str, db_user: str, db_pass: str,
-                 db_name: str) -> None:
-        self.db_host = db_host
-        self.db_port = db_port
-        self.db_user = db_user
-        self.db_pass = db_pass
-        self.db_name = db_name
+class AsyncConfig(BaseConfig):
 
     @property
     def DATABASE_URL(self) -> str:

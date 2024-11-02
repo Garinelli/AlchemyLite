055afec772dc9336124e18568a5c717818799dae
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-11-02 13:37:53 +0700

    Update async crud operations.

--- alchemylite/aasync/crud.py
+++ alchemylite/aasync/crud.py
@@ -1,7 +1,9 @@
 from typing import Any
+
 from sqlalchemy import select, update, delete
-from sqlalchemy.inspection import inspect
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.inspection import inspect
+
 
 class AsyncCrudOperation:
     def __init__(self, async_session_factory: async_sessionmaker, model, base):
@@ -21,3 +23,43 @@ class AsyncCrudOperation:
             model = self.model(**params)
             session.add(model)
             await session.commit()
+
+    async def read(self) -> tuple:
+        async with self.async_session_factory() as session:
+            query = select(self.model)
+            result = await session.execute(query)
+            result = result.all()
+            return result
+
+    async def update_by_id(self, condition: dict[str, int], params: dict[str, Any]) -> None:
+        self.validate_params(params)
+        if 'id' not in params:
+            raise ValueError(f'Parameter "id" is missing')
+        id = condition['id']
+        if type(id) is not int:
+            raise ValueError(f'Parameter "id" must be an integer')
+
+        async with self.async_session_factory() as session:
+            stmt = update(self.model).where(self.model.id == id).values(params)
+            await session.execute(stmt)
+            await session.commit()
+
+    async def delete_by_id(self, condition: dict[str, Any]) -> None:
+        if 'id' not in condition:
+            raise ValueError(f'Parameter "id" is missing')
+        id = condition['id']
+        if type(id) is not int:
+            raise ValueError(f'Parameter "id" must be an integer')
+
+        async with self.async_session_factory() as session:
+            stmt = delete(self.model).where(self.model.id == id)
+            await session.execute(stmt)
+            await session.commit()
+
+    async def create_all_tables(self) -> None:
+        async with self.async_session_factory() as session:
+            self.base.metadata.create_all(bind=session.get_bind())
+
+    async def delete_all_tables(self) -> None:
+        async with self.async_session_factory() as session:
+            self.base.metadata.drop_all(bind=session.get_bind())
